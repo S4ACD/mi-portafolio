a0785d45158e047fa0b7164295849ae505d38198
S4ACD <117605336+S4ACD@users.noreply.github.com>
2026-06-11 13:23:28 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -35,6 +35,10 @@
           '<a href="/servicios/" class="nav__drawer-link' + (isServicios ? ' nav__link--active' : '') + '">Servicios</a>' +
           '<a href="/sobre-mi/" class="nav__drawer-link' + (isSobreMi ? ' nav__link--active' : '') + '">Sobre m\u00ed</a>' +
           '<a href="/#contacto" class="btn btn--cyan">Hablemos</a>' +
+          '<a href="mailto:hosoyalexander@gmail.com" class="nav__drawer-email">' +
+            '<svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>' +
+            'hosoyalexander@gmail.com' +
+          '</a>' +
         '</div>' +
       '</nav>'
     );
